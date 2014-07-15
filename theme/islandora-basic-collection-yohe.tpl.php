a31c0e6c7ae1246d1ff09c592483e0cb44738080
jrgriffiniii <griffinj@lafayette.edu>
2014-07-15 10:50:49 -0400

    Resolving issues related to file tracking within the repository

---
+++ theme/islandora-basic-collection-yohe.tpl.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @file
+ * islandora-basic-collection.tpl.php
+ *
+ * @TODO: needs documentation about file and variables
+ */
+?>
+
+<div class="islandora islandora-basic-collection">
+  <div class="islandora-basic-collection-grid clearfix">
+
+  <div id="book-viewer">
+
+    <div id="book-viewer-pages-overlay"></div>
+
+    <?php print $viewer; ?>
+  </div>
+
+
+</div>
+</div>
